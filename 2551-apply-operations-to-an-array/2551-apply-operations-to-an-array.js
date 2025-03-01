c70d19c88bb60fd054ba7b538da39acdf20d57d0
ASWINPRABU <149613190+ASWINPRABUSIVA@users.noreply.github.com>
2025-03-01 17:52:23 +0530

    Time: 4 ms (37.25%), Space: 53.7 MB (19.12%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.js
@@ -0,0 +1,20 @@
+var applyOperations = function(nums) {
+    
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === nums[i + 1]) {
+            nums[i] *= 2;
+            nums[i + 1] = 0;
+        }
+    }
+
+    
+    let index = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            [nums[index], nums[i]] = [nums[i], nums[index]];
+            index++;
+        }
+    }
+
+    return nums;
+};
